9805162530b243b55f117cf6d46bda8b90def753
takuya hakiri <tyapi4@yahoo.co.jp>
2019-12-09 13:26:37 +0900

    fix:修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,29 @@
 class Admin::UsersController < ApplicationController
   before_action :login?
   before_action :not_admin
-  before_action :set_user, only: [:show, :edit, :destroy]
+  before_action :set_user, only: [:destroy]
 
   def new
   end
 
   def index
     @users = User.all
+  end
+
+  def destroy
+    @user.destroy
+      redirect_to root_path, notice: 'ユーザーを削除しました' 
+  end
+
+  def update
+    if @user.update(user_params)
+     redirect_to @user, notice: 'ユーザー情報を更新しました'
+    else
+      render :edit 
+    end
+  end
+
+  def edit
   end
 
   private
