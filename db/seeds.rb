2e4e370691e2502fecc94f0835b735691229c72d
takuya hakiri <tyapi4@yahoo.co.jp>
2019-12-11 12:18:22 +0900

    add:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,22 @@
 User.create!(name:  "管理者",
              email: "admin@example.jp",
-             password:  "hakiri",
-             password_confirmation: "hakiri",
+             password:  "password",
+             password_confirmation: "password",
              admin: true)
 
+status = ["高", "中", "低"]
+10.times do |i|
+User.create!(name: "test#{i + 1}",
+             email: "seed#{i + 1}@example.jp",
+             password: "password",
+             password_confirmation: "password",
+             admin: false)
 
-# 10.times do |i|
-# User.create (name:  "aaa",
-#              email: "admin@example.jp",
-#              password:  "hakiri",
-#              password_confirmation: "hakiri",
-#              admin: false)
-# end
+Task.create!(title: "user#{i +1 }",
+             content: "seeddata#{i + 1}",
+             deadline: Date.today,
+             status: status[rand(3)],
+             priority: rand(1..2),
+             user_id: User.find(i + 1).id
+             )
+end
